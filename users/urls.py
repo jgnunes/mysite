33850ac017ebd4fc930c4bf88eb8782fd9adbbe1
jgnunes <ferreiranunesjoao@gmail.com>
2018-05-08 11:30:46 -0300

    Signup view, form and templates

--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,6 @@ app_name = 'users'
 urlpatterns = [
     url(r'^dashboard/$', views.dashboard, name='dashboard'),
     url(r'^logout/$', views.LogoutView.as_view(), name='logout'),
+    url(r'^signup/$', views.SignUpView.as_view(), name='signup'),
     url('^', include('django.contrib.auth.urls')),
 ]
