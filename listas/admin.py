f101e435357bfe737fd16c44320d2a8fd26400cd
jgnunes <ferreiranunesjoao@gmail.com>
2018-05-29 19:10:31 -0300

    populate_db works perfectly, creating Tags, Questoes e Respostas;

--- listas/admin.py
+++ listas/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from . import models
 
-admin.site.register(models.Lista)
+# admin.site.register(models.Lista)
 admin.site.register(models.Questao)
 admin.site.register(models.Resposta)
 admin.site.register(models.Tag)
--- listas/models.py
+++ listas/models.py
@@ -2,15 +2,15 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
-class Lista(models.Model):
-    user = models.ForeignKey(User, related_name='lista', on_delete = models.CASCADE, blank=True, null=True)
-    generated_at = models.DateTimeField(default=timezone.now, editable=False)
-
-    def __str__(self):
-        return '{}: {}'.format(self.generated_at, self.user)
+# class Lista(models.Model):
+#     user = models.ForeignKey(User, related_name='lista', on_delete = models.CASCADE, blank=True, null=True)
+#     generated_at = models.DateTimeField(default=timezone.now, editable=False)
+#
+#     def __str__(self):
+#         return '{}: {}'.format(self.generated_at, self.user)
 
 class Questao(models.Model):
-    lista = models.ForeignKey(Lista, related_name='listas', on_delete = models.CASCADE, blank=True, null=True)
+    # lista = models.ForeignKey(Lista, related_name='listas', on_delete = models.CASCADE, blank=True, null=True)
     texto = models.CharField(max_length=500, default="")
     # tags = models.CharField(max_length=500, default="")
     data_de_upload = models.DateTimeField(default=timezone.now)
